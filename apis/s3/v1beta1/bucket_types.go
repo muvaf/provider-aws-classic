9f25fe972562a945d4317d4b718caa4c32fe3fd9
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-08-21 19:08:40 +0300

    s3 bucket: v1beta1 api structs
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

---
+++ apis/s3/v1beta1/bucket_types.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// BucketParameters are parameters for configuring the calls made to AWS Bucket API.
+type BucketParameters struct {
+}
+
+// BucketObservation is observation of Bucket properties.
+type BucketObservation struct {
+}
+
+// BucketSpec represents the desired state of the Bucket.
+type BucketSpec struct {
+	runtimev1alpha1.ResourceSpec `json:",inline"`
+	ForProvider                  BucketParameters `json:"forProvider"`
+}
+
+// BucketStatus represents the observed state of the Bucket.
+type BucketStatus struct {
+	runtimev1alpha1.ResourceStatus `json:",inline"`
+	AtProvider                     BucketObservation `json:"atProvider"`
+}
+
+// +kubebuilder:object:root=true
+
+// An Bucket is a managed resource that represents an AWS S3 Bucket.
+// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
+// +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,aws}
+type Bucket struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BucketSpec   `json:"spec"`
+	Status BucketStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// BucketList contains a list of Buckets
+type BucketList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Bucket `json:"items"`
+}
